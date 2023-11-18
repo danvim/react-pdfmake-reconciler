6766dee4c2a6118817cb88cc1c9caa148622eb33
danvim <daniel.2345@hotmail.com>
2023-11-19 02:50:16 +0800

    Fix GH Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-pdfmake/",
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
