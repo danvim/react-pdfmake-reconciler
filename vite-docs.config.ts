8aa2a31fc40f5153e6de033941a81e93f6eeadb2
danvim <daniel.2345@hotmail.com>
2023-11-24 03:11:05 +0800

    Fix demo page

--- vite-docs.config.ts
+++ vite-docs.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-pdfmake-reconciler/",
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
 });
